2cbe761cecae54cc5125b808673c6d9c9aa36b1e
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-07 16:24:35 +0000

    vars and blocks

---
+++ vars.js
@@ -0,0 +1,21 @@
+const accId=144553
+let accEmail="name@google.com"
+var accPswd="12345"
+accCity="bhavnagar"
+accEmail="hmdt@hmdt.com"
+accPswd="987456"
+accCity="rajkot"
+let accState;
+// accId=2
+console.log(accId);
+console.log(accEmail);
+console.log(accPswd);
+console.log(accCity);
+console.table ([accId,accEmail,accPswd,accCity,accState])
+
+/*don't use var oly use let as it creates block statements problem
+ for multiple users and files and variable can also be safely defined by
+ just giving it's name without let but it's not
+ preferable if we make a variable without giving its value anything
+ then on printing it will show undefined*/
+
