e2866fc1a52c5fd95f87f6c720055c77869e6c22
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-08 17:03:55 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+"use strict";//for latest version of JS
+//alert("hello")   we are using nodejs not browser so for now consider this feature to not be available
+
+/*code readabiliity is important like console.log(3+3) is better while console.log(3
++
+3)                             IT'S WEIRD RIGHT:(? so do remember this*/
+
+console.log("het");
+console.log(3+3);
+let name="thakkar";
+let age=18;
+let state;
+let isLoggedIn=false;
+/* DATATYPES 
+number till 2^53
+string ""
+bigInt
+boolean T/F
+symbol uniqueness
+                            null is a stand alone value
+                            undefined
+                            object
+*/
+
+console.log(typeof "het");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof object);
+
+
