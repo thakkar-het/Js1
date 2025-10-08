e7d56b2d21fb8fb183b687301033d62287e032ad
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-08 17:13:48 +0000

    conversions

---
+++ conversions and operations.js
@@ -0,0 +1,8 @@
+"use strict";
+let score=4;
+console.log(typeof score);
+/*just to make sure to knoe the type of variable like in case of /const {score}=req.body;/or some other unknown command*/
+
+
+
+
