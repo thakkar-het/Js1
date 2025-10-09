f84eea2923dbf17736bdc43fcebdcebe06928515
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-09 05:20:10 +0000

    conversions

--- conversions and operations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let score=4;
-console.log(typeof score);
-/*just to make sure to knoe the type of variable like in case of /const {score}=req.body;/or some other unknown command*/
-
-
-
-
---
+++ conversions.js
@@ -0,0 +1,58 @@
+let answer="44"
+console.log(typeof answer);
+let valInNum=Number(answer)
+console.log(typeof valInNum);
+console.log(valInNum);//44
+
+
+let score="11xyz";
+console.log(typeof score);
+let value=Number(score)
+console.log(typeof value);
+console.log(value);//NaN  not a number
+
+let something=null
+console.log(typeof something);
+let num=Number(something)
+console.log(typeof num);
+console.log(num);//0
+
+let idk=undefined
+console.log(typeof idk);
+let number=Number(idk)
+console.log(typeof number);
+console.log(number);//NaN
+
+let what=true
+console.log(typeof what);
+let val=Number(what)
+console.log(typeof val);
+console.log(val);//1 for true 0 for false
+
+let why="het"
+console.log(typeof why);
+let valid=Number(why)
+console.log(typeof valid);
+console.log(valid);//NaN
+
+let log=1
+let booleanlog=Boolean(log)
+console.log(typeof booleanlog);
+console.log(booleanlog);//true for 1 and false for 0
+
+let logg=""
+let booleanlogg=Boolean(logg)
+console.log(typeof booleanlogg);
+console.log(booleanlogg);//false
+
+let loggg="het"
+let booleanloggg=Boolean(loggg)
+console.log(typeof booleanloggg);
+console.log(booleanloggg);//true
+
+let lo=1
+let booleanlo=Boolean(lo)
+console.log(typeof booleanlo);
+console.log(booleanlo);
+
+
