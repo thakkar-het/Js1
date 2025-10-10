4007d359a12ede1c5cf256c04e1651ae9a0c342b
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-10 15:12:33 +0000

    n

---
+++ operations.js
@@ -0,0 +1,53 @@
+//general operations
+let val=3;
+let neg=-val;
+// console.log(neg);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//exponential
+// console.log(2/2);
+// console.log(2%2);//modulus or remainder
+
+// to concate strings
+let str1="hello";
+let str2=" het";
+let str3=str1+str2;
+console.log(str3);
+
+
+// //tricky operations
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");//remember the rule for operations
+
+// //extra from vs code side
+
+// console.log("1"-2);
+// console.log(1-"2");
+// console.log("1"-2+2);
+// console.log(1+2-"2");//remember the rule for operations
+
+// console.log(true);
+// console.log(+true);//1
+// ///console.log(true+);//error
+// console.log(+"");//0
+// ///console.log(""+);//error
+
+// let counter=100;
+// counter++;
+// console.log(counter);//101
+// ++counter;
+// console.log(counter);//102
+
+
+let x=3,y;
+y=x++;
+console.log(x,y);//4,3
+
+let a=3,b;
+b=++a;
+console.log(a,b);//4,4
+
