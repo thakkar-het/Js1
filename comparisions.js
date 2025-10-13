4faf2c770311ee06210826cf5c06b79271a5d79f
thakkar-het <b25cm1057@iitj.ac.in>
2025-10-13 13:58:11 +0000

    comparisions

---
+++ comparisions.js
@@ -0,0 +1,38 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+// we should always use same datatypes while comparing or the results will not be predictable
+//console.log("2">1);
+//console.log("02">1);
+//console.log("01"==1);
+
+//console.log(null==0);
+//console.log(null>=0);
+//console.log(null>0);
+//console.log(null<=0);
+//console.log(null<0);
+
+// while comparision of <,>,<=,>= is done it converts null to 0 but while comparing with == it does not convert null to 0
+//console.log(undefined==0);
+//console.log(undefined>=0);
+//console.log(undefined>0);
+//console.log(undefined<=0);
+//console.log(undefined<0);
+
+//console.log(null==undefined);
+//console.log(NaN==NaN);
+
+// === is strict equality operator
+
+console.log(2==="2");
+console.log(0===false);
+console.log(1===true);
+console.log(null===undefined);
+console.log(NaN===NaN);
+
+
