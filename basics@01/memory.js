7d53d841a463c98cf4e9a8a5765f313a7e8a52f7
thakkar-het <b25cm1057@iitj.ac.in>
2025-12-14 07:54:09 +0000

    stack and heap

--- basics@01/memory.js
+++ basics@01/memory.js
@@ -1,16 +1,16 @@
 // stack memory(primitive , copy) and heap memory(non-primitive , reference)
 
 //stack
-let familyName= "Het's Family"
-let SameFamily= familyName
+let familyName= "Het's Family";
+let SameFamily= familyName;
 
-console.log(familyName) 
-console.log(SameFamily) 
+console.log(familyName) ;
+console.log(SameFamily) ;
 
-familyName= "ommy and oddy 's Family"
+familyName= "ommy and oddy 's Family";
 
-console.log(familyName) 
-console.log(SameFamily)
+console.log(familyName) ;
+console.log(SameFamily);
 //original value of familyName is not changed because 
 // only the copy of the value is stored in SameFamily and not in the original variable
 
@@ -19,12 +19,12 @@ console.log(SameFamily)
 let user1={
     email:"user@something.com", 
     upi:"user@bank"
-}
-let user2=user1
+};
+let user2=user1;
 //here user2 is not a copy of user1
 //it is a reference to the same object in the heap memory
 
-user2.email="het@google.com"
-console.log(user1.email) 
-console.log(user2.email)
+user2.email="het@google.com";
+console.log(user1.email) ;
+console.log(user2.email);
 //both user1 and user2 email is changed because both are pointing to the same object in the heap memory
