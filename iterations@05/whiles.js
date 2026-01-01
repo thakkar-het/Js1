39cb7f343ade28520a43ece71efac585747558bc
thakkar-het <b25cm1057@iitj.ac.in>
2026-01-01 08:35:27 +0000

    while and do while loops

---
+++ iterations@05/whiles.js
@@ -0,0 +1,34 @@
+// while (condition) 
+// {
+    
+// }
+
+let i, j;
+i = 0;
+while(i <= 10)
+{
+    // console.log(`${i}`);
+    i += 2;
+}
+
+let myarr = ["h", "e", "t", "d", "m", "hehe"];
+i = 0;
+while(i < myarr.length)
+{
+    // console.log(myarr[i]);
+    i++;
+}
+
+
+// do {
+    
+// } while (condition);
+i = 1;
+do 
+{
+    console.log(`score is ${i}`);
+    i++;
+} 
+while (i <= 10);
+// if instead of 1 the value of i is set to be greater than 10 then also the loop will run 1 time this says that 
+// regardless of while condition the loop executes at least once
